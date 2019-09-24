bda15eaa167c5244c5afa70d37887d19fe6aa6a9
Dixie <adixie@iu.edu>
2019-09-23 23:48:47 -0400

    Finished task 2

--- main2.py
+++ main2.py
@@ -46,12 +46,22 @@ class MyGame(arcade.Window):
 
     def setup(self):
         self.animal_sprite = arcade.Sprite("assets/crocodile.png", 0.5)
-        self.animal_sprite.center_x = 400
+        self.animal_sprite.center_x = 320
         self.animal_sprite.center_y = 300
         self.animal_list.append(self.animal_sprite)
 
         self.emote_sprite = Emote()
-        self.emote_sprite.center_x = 400
+        self.emote_sprite.center_x = 320
+        self.emote_sprite.center_y = 360
+        self.emote_list.append(self.emote_sprite)
+
+        self.animal_sprite = arcade.Sprite("assets/bear.png", 0.5)
+        self.animal_sprite.center_x = 420
+        self.animal_sprite.center_y = 300
+        self.animal_list.append(self.animal_sprite)
+
+        self.emote_sprite = Emote()
+        self.emote_sprite.center_x = 420
         self.emote_sprite.center_y = 360
         self.emote_list.append(self.emote_sprite)
         
